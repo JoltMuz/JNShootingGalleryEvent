f7839b82190f8d741877c1807a9178ca4b1cb054
JoltTheBolt <93381290+JoltMuz@users.noreply.github.com>
2022-01-06 18:50:51 +0500

    Update Jetpack.java

--- Jetpack.java
+++ Jetpack.java
@@ -2,71 +2,85 @@ package io.github.JoltMuz.joltitems;
 
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Effect;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
+import java.util.HashMap;
+
 public class Jetpack implements Listener
 {
-    int fuel = 0;
+    int fuel;
+    HashMap<String, Integer> Fuels = new HashMap<>();
 
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e)
+    {
+        Fuels.put(e.getPlayer().getDisplayName(),0);
+
+    }
     @EventHandler
     public void Move(PlayerMoveEvent e)
     {
 
         Player p = e.getPlayer();
 
-        ItemStack chest = e.getPlayer().getInventory().getChestplate();
-        if (chest.getType() != null && chest.getType() == Material.DIAMOND_CHESTPLATE)
-        {
-            if (chest.hasItemMeta() &&
-                    chest.getItemMeta().hasDisplayName() &&
-                    chest.getItemMeta().getDisplayName().equals(ChatColor.BLUE + ChatColor.BOLD.toString() + "Jetpack"))
+        if (p.getGameMode() == GameMode.SURVIVAL){
+            if (p.getInventory().getChestplate() != null)
             {
-                new BukkitRunnable() {
-                    @Override
-                    public void run() {
+                if (p.getInventory().getChestplate().getItemMeta().getDisplayName().equals(ChatColor.BLUE + ChatColor.BOLD.toString() + "Jetpack"))
+                {
+                    new BukkitRunnable()
+                    {
+                        @Override
+                        public void run()
                         {
-                            if(p.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() != Material.AIR)
                             {
-                                if (p.isSneaking())
+                                if(p.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() != Material.AIR)
                                 {
-                                    if (fuel < 100)
+                                    if (p.isSneaking())
                                     {
-                                        fuel = fuel + 1;
-                                        new ActionBar(ChatColor.YELLOW + ChatColor.BOLD.toString() + new String(new char[fuel / 5]).replace("\0", "▇")).sendToPlayer(p);
+
+                                        if ((Integer)Fuels.get(e.getPlayer().getDisplayName()) < 100)
+                                        {
+                                            Fuels.put((e.getPlayer().getDisplayName()),Fuels.get(e.getPlayer().getDisplayName()) + 1);
+                                            new ActionBar(ChatColor.YELLOW + ChatColor.BOLD.toString() + new String(new char[Fuels.get(e.getPlayer().getDisplayName()) / 5]).replace("\0", "▇")).sendToPlayer(p);
+                                        }
+                                        if ((Integer)Fuels.get(e.getPlayer().getDisplayName()) == 100)
+                                        {
+                                            new ActionBar(ChatColor.GOLD + ChatColor.BOLD.toString() + "Fuel Fully Recharged!").sendToPlayer(p);
+                                            this.cancel();
+                                        }
                                     }
-                                    if (fuel == 100)
+                                    if ((Integer)Fuels.get(e.getPlayer().getDisplayName()) == 0)
                                     {
-                                        new ActionBar(ChatColor.GOLD + ChatColor.BOLD.toString() + "Fuel Fully Recharged!").sendToPlayer(p);
-                                        this.cancel();
+                                        p.setFlying(false);
+                                        p.setAllowFlight(false);
+                                        new ActionBar(ChatColor.DARK_GRAY + ChatColor.BOLD.toString() + "Sneak to Recharge Fuel").sendToPlayer(p);
                                     }
-                                }
-                                if (fuel == 0) {
-                                    p.setFlying(false);
-                                    p.setAllowFlight(false);
-                                    new ActionBar(ChatColor.DARK_GRAY + ChatColor.BOLD.toString() + "Sneak to Recharge Fuel").sendToPlayer(p);
                                 }
                             }
                         }
-                    }
-                }.runTaskTimer(Main.plugin, 80L, 100L);
+                    }.runTaskTimer(Main.plugin, 80L, 100L);
 
-                if (p.getVelocity().getY() > 0)
-                {
-
-                    if (fuel == 100)
+                    if (p.getVelocity().getY() > 0)
                     {
-                        p.setAllowFlight(true);
 
+                        if ((Integer)Fuels.get(e.getPlayer().getDisplayName()) == 100)
+                        {
+                            p.setAllowFlight(true);
+
+                        }
                     }
                 }
             }
@@ -76,34 +90,36 @@ public class Jetpack implements Listener
     public void Fly(PlayerToggleFlightEvent e)
     {
         Player p = e.getPlayer();
-        if (fuel == 100)
-        {
-            new BukkitRunnable()
+        if (p.getGameMode() == GameMode.SURVIVAL){
+            if ((Integer)Fuels.get(e.getPlayer().getDisplayName()) == 100)
             {
-                @Override
-                public void run()
+                new BukkitRunnable()
                 {
-                    fuel = fuel - 10;
-                    p.setVelocity(p.getVelocity().add(new Vector(0, 0.5, 0)));
-                    p.setVelocity(p.getLocation().getDirection().multiply(1.5));
-                    if (!p.isFlying()) {p.setAllowFlight(true);p.setFlying(true);}
-                    new ActionBar(ChatColor.YELLOW + ChatColor.BOLD.toString() + new String(new char[fuel / 5]).replace("\0", "▇")).sendToPlayer(p);
-                    p.getWorld().playEffect(p.getLocation().add(new Vector(0,1,0)) ,Effect.LAVA_POP,  0);
-                    p.getWorld().playEffect(p.getLocation().add(new Vector(0,1,0)) ,Effect.LAVADRIP,  0);
-                    if (fuel == 0)
+                    @Override
+                    public void run()
                     {
-                        p.setAllowFlight(false);
-                        p.setFlying(false);
-                        this.cancel();
+                        Fuels.put((e.getPlayer().getDisplayName()),Fuels.get(e.getPlayer().getDisplayName()) - 10);;
+                        p.setVelocity(p.getVelocity().add(new Vector(0, 0.5, 0)));
+                        p.setVelocity(p.getLocation().getDirection().multiply(1.5));
+                        if (!p.isFlying()) {p.setAllowFlight(true);p.setFlying(true);}
+                        new ActionBar(ChatColor.YELLOW + ChatColor.BOLD.toString() + new String(new char[(Integer)Fuels.get(e.getPlayer().getDisplayName()) / 5]).replace("\0", "▇")).sendToPlayer(p);
+                        p.getWorld().playEffect(p.getLocation().add(new Vector(0,1,0)) ,Effect.LAVA_POP,  0);
+                        p.getWorld().playEffect(p.getLocation().add(new Vector(0,1,0)) ,Effect.LAVADRIP,  0);
+                        if ((Integer)Fuels.get(e.getPlayer().getDisplayName()) == 0)
+                        {
+                            p.setAllowFlight(false);
+                            p.setFlying(false);
+                            this.cancel();
+                        }
+                        if(p.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() != Material.AIR)
+                        {
+                            p.setAllowFlight(false);
+                            p.setFlying(false);
+                            this.cancel();
+                        }
                     }
-                    if(p.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() != Material.AIR)
-                    {
-                        p.setAllowFlight(false);
-                        p.setFlying(false);
-                        this.cancel();
-                    }
-                }
-            }.runTaskTimer(Main.plugin, 0L, 2L);
+                }.runTaskTimer(Main.plugin, 0L, 2L);
+            }
         }
     }
     @EventHandler
