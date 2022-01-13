4792058c6b24fe61e6d5ffba0103df993e86e50f
JoltTheBolt <93381290+JoltMuz@users.noreply.github.com>
2022-01-13 11:51:21 +0500

    Update Wand.java

--- Wand.java
+++ Wand.java
@@ -7,32 +7,34 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
 
 import java.util.Set;
 
+import static io.github.JoltMuz.joltitems.Cooldown.*;
+
 public class Wand implements Listener
 {
+
     @EventHandler
-    public void onPlayerInteractBlock(PlayerInteractEvent e)
+    private void onPlayerInteract(PlayerInteractEvent e)
     {
         Action action = e.getAction();
 
-        if (action == Action.RIGHT_CLICK_BLOCK || action == Action.RIGHT_CLICK_AIR)
-        {
-
-            Player p = e.getPlayer();
-            ItemStack item = p.getInventory().getItemInHand();
-            if (p.getItemInHand().getType() == Material.STICK)
+    if (action == Action.RIGHT_CLICK_BLOCK || action == Action.RIGHT_CLICK_AIR)
+    {
+        Player p = e.getPlayer();
+        if (CheckItem.CheckItem(p.getInventory().getItemInHand(),Material.STICK, ChatColor.AQUA + ChatColor.BOLD.toString() + "Lightning Wand"))
             {
-
-                if(item.hasItemMeta() &&
-                item.getItemMeta().hasDisplayName() &&
-                item.getItemMeta().getDisplayName().equals(ChatColor.AQUA + ChatColor.BOLD.toString() + "Lightning Wand"))
+                if (checkCooldown(p.getName(), wand, 2))
                 {
 
                     p.getWorld().strikeLightning(p.getTargetBlock((Set<Material>) null, 200).getLocation());
 
+                }
+                else
+                {
+                    double cooldown = 2 - ((double)System.currentTimeMillis()/(double)1000 - (Double) wand.get(p.getName()));
+                    p.sendMessage(ChatColor.RED + "Lightning Wand Cooldown: " + Math.round(cooldown*10.0)/10.0 + " second(s).");
                 }
             }
         }
