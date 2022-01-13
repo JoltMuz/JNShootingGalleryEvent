0c3b9c6ea10a99ab73ac9c27a4d4358fee40e87c
JoltTheBolt <93381290+JoltMuz@users.noreply.github.com>
2022-01-13 11:47:45 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,35 @@
 package io.github.JoltMuz.joltitems;
 
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 public final class Main extends JavaPlugin implements Listener
 {
+    public static Plugin plugin;
+
+
     @Override
     public void onEnable()
     {
+        plugin = this;
         getServer().getPluginManager().registerEvents( new UI(), this);
         getServer().getPluginManager().registerEvents( new Incinerator(), this);
         getServer().getPluginManager().registerEvents( new Wand(), this);
         getServer().getPluginManager().registerEvents( new Pogo(), this);
         getServer().getPluginManager().registerEvents( new Swifts(), this);
-        
+        getServer().getPluginManager().registerEvents( new Jetpack(), this);
+        getServer().getPluginManager().registerEvents( new Railgun(), this);
+        getServer().getPluginManager().registerEvents( new YonduFin(), this);
+        getServer().getPluginManager().registerEvents( new Bustercall(), this);
+        getServer().getPluginManager().registerEvents( new BridgeEgg(), this);
+        getServer().getPluginManager().registerEvents( new Grapplinghook (), this);
+
         this.getCommand("joltitems").setExecutor(new UI());
-    }
+
+
+        };
     @Override
     public void onDisable()
     {
