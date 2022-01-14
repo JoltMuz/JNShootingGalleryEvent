83dc099429b8f66003a8266f57e7983df1aeed93
JoltTheBolt <93381290+JoltMuz@users.noreply.github.com>
2022-01-14 19:12:54 +0500

    Update Railgun.java

--- Railgun.java
+++ Railgun.java
@@ -1,18 +1,14 @@
 package io.github.JoltMuz.joltitems;
 
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Effect;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class Railgun implements Listener
@@ -29,13 +25,25 @@ public class Railgun implements Listener
             World w = p.getWorld();
             if(CheckItem.CheckItem(p.getItemInHand(), Material.GOLD_HOE, ChatColor.DARK_RED + ChatColor.BOLD.toString() + "Railgun"))
             {
-                w.playEffect(p.getTargetBlock((Set<Material>) null, 2).getLocation(), Effect.HEART,null);
-                w.playEffect(p.getTargetBlock((Set<Material>) null, 3).getLocation(), Effect.HEART,null);
-                w.playEffect(p.getTargetBlock((Set<Material>) null, 4).getLocation(), Effect.HEART,null);
-                w.playEffect(p.getTargetBlock((Set<Material>) null, 5).getLocation(), Effect.HEART,null);
-                w.playEffect(p.getTargetBlock((Set<Material>) null, 6).getLocation(), Effect.HEART,null);
-                w.playEffect(p.getTargetBlock((Set<Material>) null, 7).getLocation(), Effect.HEART,null);
-                w.playEffect(p.getTargetBlock((Set<Material>) null, 8).getLocation(), Effect.HEART,null);
+                ArrayList<Location> Locations = new ArrayList<>();
+                for ( int i =2 ; i < 11; i += 1)
+                {
+                    Locations.add(p.getTargetBlock((Set<Material>) null, i).getLocation());
+                }
+                for ( int i = 0 ; i < Locations.size(); i ++)
+                {
+                    w.playEffect(Locations.get(i), Effect.HEART, null);
+                    for (Entity en : Locations.get(i).getChunk().getEntities())
+                    {
+                        if (en.getLocation().distance(Locations.get(i)) < 1.5)
+                        {
+                            if (en instanceof Damageable)
+                            {
+                                ((Damageable) en).damage(5);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
